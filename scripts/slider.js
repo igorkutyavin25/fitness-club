e362926d50b49e8c18471120963e0ae7ffce2a03
Игорь Кутявин <igorkutyavin@192.168.1.3>
2024-04-11 21:23:54 +0300

    new file

---
+++ scripts/slider.js
@@ -0,0 +1,24 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    slidesPerView: 5,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 2,
+        },
+        952: {
+            slidesPerView: 3,
+        },
+        1200: {
+            slidesPerView: 4,
+        },
+        1400: {
+            slidesPerView: 5,
+        }
+      }
+  });
